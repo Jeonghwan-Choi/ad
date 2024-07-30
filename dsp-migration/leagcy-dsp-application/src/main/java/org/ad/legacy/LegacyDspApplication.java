2576471bef7a452f03ae87397a6f7a8a5d3de688
jack <yzon007@naver.com>
2024-07-30 15:46:52 +0900

    JPA 처음부터 다시 설정 적용해보기

---
+++ dsp-migration/leagcy-dsp-application/src/main/java/org/ad/legacy/LegacyDspApplication.java
@@ -0,0 +1,12 @@
+package org.ad.legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyDspApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LegacyDspApplication.class, args);
+  }
+}
---
+++ dsp-migration/leagcy-dsp-application/src/main/java/org/ad/legacy/domain/AdGroup.java
@@ -0,0 +1,19 @@
+package org.ad.legacy.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class AdGroup {
+
+  @Id
+  private Long id;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
